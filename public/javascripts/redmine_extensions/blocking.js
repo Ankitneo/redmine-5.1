b75fcffc1f70b12a58ead299a7ef48d4c869005a
Ankitneo <gupta.ankit@neosoftmail.com>
2024-02-15 10:36:05 +0530

    added project

---
+++ public/javascripts/redmine_extensions/blocking.js
@@ -0,0 +1,8 @@
+/**
+ * code, that cannot be delayed after window.onload or deferred
+ */
+//= require ./blocking_namespace
+//= require ./blocking_module
+//= require ./blocking_polyfill
+//= require ./blocking_utils
+//= require ./blocking_dynamic_loading
---
+++ public/javascripts/redmine_extensions/blocking_namespace.js
@@ -0,0 +1,4 @@
+window.EASY = window.EASY || {};
+window.EasyGem = window.EasyGem || {};
+window.EASY.asyncModules = window.EASY.asyncModules || {};
+window.EASY.asyncModules.states = window.EASY.asyncModules.states || {};
